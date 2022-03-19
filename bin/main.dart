2a3a9f3f6959a2dc285eab051f5ec82c1b3feb4c
Klemen Tušar <techouse@gmail.com>
2022-03-19 22:31:22 +0000

    :sparkles: Add automatic update

--- bin/main.dart
+++ bin/main.dart
@@ -6,6 +6,7 @@ import 'package:alfred_workflow/alfred_workflow.dart'
         AlfredItemIcon,
         AlfredItemText,
         AlfredItems,
+        AlfredUpdater,
         AlfredWorkflow;
 import 'package:algolia/algolia.dart' show AlgoliaQuerySnapshot;
 import 'package:args/args.dart' show ArgParser, ArgResults;
@@ -20,8 +21,13 @@ import 'src/services/algolia_search.dart' show AlgoliaSearch;
 final HtmlUnescape unescape = HtmlUnescape();
 
 final AlfredWorkflow workflow = AlfredWorkflow();
-
+final AlfredUpdater updater = AlfredUpdater(
+  githubRepositoryUrl: Config.githubRepositoryUrl,
+  currentVersion: Config.version,
+  updateInterval: Duration(days: 7),
+);
 bool verbose = false;
+bool update = false;
 
 void main(List<String> arguments) async {
   try {
@@ -30,17 +36,24 @@ void main(List<String> arguments) async {
     workflow.clearItems();
 
     final ArgParser parser = ArgParser()
-      ..addOption('query', abbr: 'q', mandatory: true)
-      ..addFlag('verbose', abbr: 'v', defaultsTo: false);
+      ..addOption('query', abbr: 'q', defaultsTo: '')
+      ..addFlag('verbose', abbr: 'v', defaultsTo: false)
+      ..addFlag('update', abbr: 'u', defaultsTo: false);
     final ArgResults args = parser.parse(arguments);
+
+    update = args['update'];
+    if (update) {
+      stdout.writeln('Updating workflow...');
+
+      return await updater.update();
+    }
 
     verbose = args['verbose'];
 
     List<String> query =
         args['query'].replaceAll(RegExp(r'\s+'), ' ').trim().split(' ');
-    String? version = query.firstWhereOrNull(
-      (el) => Config.supportedVersions.contains(el),
-    );
+    String? version =
+        query.firstWhereOrNull((el) => Config.supportedVersions.contains(el));
     if (version != null) {
       query.removeWhere((str) => str == version);
     } else {
@@ -64,13 +77,27 @@ void main(List<String> arguments) async {
   } catch (err) {
     exitCode = 1;
     workflow.addItem(AlfredItem(title: err.toString()));
-    if (verbose) {
-      rethrow;
-    }
+    if (verbose) rethrow;
   } finally {
-    workflow.run();
+    if (!update) {
+      if (await updater.updateAvailable()) {
+        workflow.run(addToBeginning: updateItem);
+      } else {
+        workflow.run();
+      }
+    }
   }
 }
+
+const updateItem = AlfredItem(
+  title: 'Auto-Update available!',
+  subtitle: 'Press <enter> to auto-update to a new version of this workflow.',
+  arg: 'update:workflow',
+  match:
+      'Auto-Update available! Press <enter> to auto-update to a new version of this workflow.',
+  icon: AlfredItemIcon(path: 'alfredhatcog.png'),
+  valid: true,
+);
 
 void _showPlaceholder() {
   workflow.addItem(
--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,6 +1,9 @@
 class Config {
   Config._();
 
+  static const String version = '2.0.3';
+  static final Uri githubRepositoryUrl =
+  Uri.https('github.com', '/techouse/alfred-vue-docs');
   static const String algoliaApplicationId = 'ML0LEBN7FQ';
   static const String algoliaSearchOnlyApiKey =
       'f49cbd92a74532cc55cfbffa5e5a7d01';
